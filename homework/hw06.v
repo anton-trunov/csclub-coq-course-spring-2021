9870ab93b76f1b785dde2f5a167d83dd5311e649
Anton Trunov <anton.a.trunov@gmail.com>
2021-05-21 15:54:03 +0300

    fix: homework 6

--- homework/hw06.v
+++ homework/hw06.v
@@ -26,9 +26,9 @@ Fixpoint eval (e : expr) : nat :=
 Fixpoint eval (e : expr) : nat :=
   match e with
   | Const n => n
-  | Plus e1 e2 => eval_expr e1 + eval_expr e2
-  | Minus e1 e2 => eval_expr e1 - eval_expr e2
-  | Mult e1 e2 => eval_expr e1 * eval_expr e2
+  | Plus e1 e2 => eval e1 + eval e2
+  | Minus e1 e2 => eval e1 - eval e2
+  | Mult e1 e2 => eval e1 * eval e2
   end.
 
 *)
